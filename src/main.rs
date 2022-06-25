37e44bfc706cb8cd8ee093e2f8f6172846ceb0b9
Stefan Lankes <slankes@eonerc.rwth-aachen.de>
2022-06-25 14:56:11 +0200

    add httpd demo

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,19 @@
 #[cfg(target_os = "hermit")]
 use hermit_sys as _;
+use tiny_http::{Response, Server};
 
 fn main() {
-	println!("Hello World!");
+	let server = Server::http("0.0.0.0:8000").unwrap();
+
+	for request in server.incoming_requests() {
+		println!(
+			"received request! method: {:?}, url: {:?}, headers: {:?}",
+			request.method(),
+			request.url(),
+			request.headers()
+		);
+
+		let response = Response::from_string("hello world");
+		request.respond(response);
+	}
 }
